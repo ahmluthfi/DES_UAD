bfb383db53de6327bd0e0265ddb6ca440419cfdc
LaluBRamadhan <lalubagoes2@gmail.com>
2024-06-10 09:32:30 +0700

    added navbar

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: , prefer_const_constructors, prefer_const_literals_to_create_immutables
+import 'package:des_uad/init_screens/fragment_view.dart';
 import 'package:flutter/material.dart';
 
 import '../../core/constant_finals.dart';
